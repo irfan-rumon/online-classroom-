ab658e3b72fea36e1a0a0bd8bcf2079f1a13c9be
irfan-rumon <irfannisho8571@gmail.com>
2022-02-10 19:35:49 +0600

    registration feature added

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,10 +14,31 @@
                 <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             </div>
 
+
+            
+
             <div class="mt-4">
                 <x-jet-label for="email" value="{{ __('Email') }}" />
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
             </div>
+
+            
+           
+
+            <div>
+                <x-jet-label for="role" value="{{ __('role') }}" />
+                <select id="role"  class="block mt-1 w-full" name="role">
+
+                <option value="customer"  >
+                       Teacher
+                </option>
+                <option value="electrician">
+                       Student
+                </option>
+                
+
+            </select>
+        </div>
 
             <div class="mt-4">
                 <x-jet-label for="password" value="{{ __('Password') }}" />
