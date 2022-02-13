9061f2eb228145f6b632adc6e1b68128e55643fe
irfan-rumon <irfannisho8571@gmail.com>
2022-02-13 23:04:28 +0600

    Submission model created with migration

---
+++ database/migrations/2022_02_13_170241_create_submissions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('submissions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('assignment_id');
+            $table->string('student_id');
+            $table->string('answer');
+            $table->integer('score');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('submissions');
+    }
+};
