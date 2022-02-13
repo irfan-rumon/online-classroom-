671bec1befb1b735b6da2ed0a69349b854ad09e5
irfan-rumon <irfannisho8571@gmail.com>
2022-02-13 22:56:22 +0600

    room class created with migration

--- database/migrations/2022_02_13_165411_create_rooms_table.php
+++ database/migrations/2022_02_13_165411_create_rooms_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('matterials', function (Blueprint $table) {
+        Schema::create('rooms', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('course_id')->nullable();
-            $table->string('material')->nullable();
+            $table->string('teacher_id');
+            $table->string('room_code');
         });
     }
 
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('matterials');
+        Schema::dropIfExists('rooms');
     }
 };
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\RoomController;
+use App\Http\Controllers\StudyMaterialController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,12 +21,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard/t', [TeacherController::class, 'index'])->middleware('auth');
-Route::get('/create-class', [RoomController::class, 'create'])->middleware('auth');
-Route::get('/class/{id}', [RoomController::class, 'enter'])->middleware('auth');
-Route::get('/show-classes', [RoomController::class, 'show'])->middleware('auth');
-Route::post('/store-class', [RoomController::class, 'store'])->middleware('auth');
+Route::get('/dashboard', [UserController::class, 'index'])->middleware('auth');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+
