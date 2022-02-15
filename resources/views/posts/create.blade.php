506ec028e8d1ed230614bf850121591a74ea5c66
irfan-rumon <irfannisho8571@gmail.com>
2022-02-15 21:06:02 +0600

    create form added

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create Post</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+<form action="{{ url('post/store')  }}" method="POST">
+  <div class="mb-3">
+    <label for="topic" class="form-label">Topic:</label>
+    <input type="text" class="form-control" id="topic" name="topic" aria-describedby="topic">
+    
+  </div>
+
+  <div class="mb-3">
+    <label for="contents" class="form-label">Contents:</label>
+    <input type="textarea" class="form-control" id="contents" name="contents" aria-describedby="topic">
+    
+  </div>
+ 
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</body>
+</html>
---
+++ resources/views/rooms/create.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create Room</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+<form action="{{ url('room/store')  }}" method="POST">
+  <div class="mb-3">
+    <label for="room_code" class="form-label">Room Code:</label>
+    <input type="text" class="form-control" id="room_code" name="room_code" aria-describedby="room_code">
+    
+  </div>
+ 
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</body>
+</html>
